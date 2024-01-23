eb93e6764001ac980bf8d90904b8737da4719128
AlexanderPAI <lawlikeart@yandex.ru>
2024-01-23 13:43:13 +0300

    feat: Docker-compose and nginx

--- currency_rates/currency_rates/settings.py
+++ currency_rates/currency_rates/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -117,3 +117,4 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
