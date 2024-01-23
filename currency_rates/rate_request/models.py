77118352dc80c0d913055645f1c6ecb845202253
AlexanderPAI <lawlikeart@yandex.ru>
2024-01-23 04:02:17 +0300

    fix: fields currency_name, target_currency have been deleted from model RateRequest

--- currency_rates/rate_request/models.py
+++ currency_rates/rate_request/models.py
@@ -8,20 +8,10 @@ class RateRequest(models.Model):
         auto_now_add=True,
         db_index=True,
     )
-    currency_name = models.CharField(
-        verbose_name='Название валюты 1',
-        help_text='Полное название валюты 1 (например, "Доллар США")',
-        max_length=200,
-    )
     currency_code = models.CharField(
         verbose_name='Код валюты 1',
         help_text='Международный код валюты 1 в соответствии с ISO 4217 (Общепринятый стандарт)',
         max_length=3,
-    )
-    target_currency = models.CharField(
-        verbose_name='Название валюты 2',
-        help_text='Полное название валюты 2 (например, "Российский рубль")',
-        max_length=200,
     )
     target_currency_code = models.CharField(
         verbose_name='Код валюты 2',
