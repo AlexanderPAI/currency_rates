409ddfa106582b5a6391bf28431172df1d12a09c
AlexanderPAI <lawlikeart@yandex.ru>
2024-01-23 06:48:48 +0300

    feat: GET-request return current rate usd and 10 last requests at endpoint get-current-usd

--- currency_rates/api/serializers.py
+++ currency_rates/api/serializers.py
@@ -3,9 +3,8 @@ from rest_framework import serializers
 from rate_request.models import RateRequest
 
 
-class RateRequestSerializer(serializers.ModelSerializer):
-    """Сериализатор запроса курса валюты."""
-    # recent_requests = serializers.SerializerMethodField()
+class RateRequestListSerializer(serializers.ModelSerializer):
+    """Сериализатор списка запросов курсов валют."""
 
     class Meta:
         model = RateRequest
@@ -14,8 +13,24 @@ class RateRequestSerializer(serializers.ModelSerializer):
             'currency_code',
             'target_currency_code',
             'rate',
-            # 'recent_requests'
         )
 
-    def get_history(self, obj):
-        pass
+
+class RateRequestSerializer(serializers.ModelSerializer):
+    """Сериализатор запроса текущего курса валюты с историей запросов."""
+    recent_requests = serializers.SerializerMethodField()
+
+    class Meta:
+        model = RateRequest
+        fields = (
+            'request_date',
+            'currency_code',
+            'target_currency_code',
+            'rate',
+            'recent_requests',
+        )
+
+    def get_recent_requests(self, obj):
+        data = RateRequest.objects.all()[:10]
+        serializer = RateRequestListSerializer(data, many=True)
+        return serializer.data
--- currency_rates/api/services/get_usd_from_api.py
+++ currency_rates/api/services/get_usd_from_api.py
@@ -6,10 +6,6 @@ from pathlib import Path
 from requests import get
 
 
-env_file = Path(__file__).parent.parent.parent / 'currency_rates' / '.env'
-load_dotenv(env_file)
-
-
 def get_currency_rate(endpoint: str, app_id: str, currency: str, target_currency: str) -> dict or None:
     """Функция для запроса курса валюты с https://openexchangerates.org"""
     rate_request = f'{endpoint}?app_id={app_id}&base={currency}&symbols={target_currency}'
